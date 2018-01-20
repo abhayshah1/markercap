bdb768a2051cee569e3ff6fbb91f5c3a1e141dc2
abhayshah1 <net_abhay@yahoo.com>
2018-01-19 22:25:44 -0500

    Reorganized code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import UpgradeLineChart from './UpgradeLineChart';
+import UpgradeLineChart from './components/UpgradeLineChart';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<UpgradeLineChart />, document.getElementById('root'));
