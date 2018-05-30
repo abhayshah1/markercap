20f7b598a7bdcd5a9dff62b8bf4f5598c5adb2eb
abhayshah1 <net_abhay@yahoo.com>
2018-05-29 21:38:44 -0400

    Added dotenv dependency

--- src/components/DeviationLineChart.jsx
+++ src/components/DeviationLineChart.jsx
@@ -55,7 +55,7 @@ class DeviationLineChart extends Component {
 		var newDataSets = [];
 		if ( nextProps.companyID === 'FNWL' ) {
 			// get data for FNWL
-			axios.get('http://localhost:3001/markercap/FNWL')
+			axios.get(process.env.REACT_APP_MARKERCAP_URL+'/FNWL')
 				.then( res => {
 					farmersData.data = res.data.data;
 				});
